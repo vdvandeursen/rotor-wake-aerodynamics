9e1a999a089f262717dae752935dcc0318f7f998
klecki98 <kacper.lecki@gmail.com>
2023-03-25 14:56:13 +0100

    Optimized chord distribution

--- main.py
+++ main.py
@@ -68,49 +68,100 @@ if __name__ == '__main__':
     performance_df = pd.DataFrame(results)
 
     print(performance_df)
+    if False:
+        results = []
 
-    results = []
+        #exponents=[0.005,0.02,0.05,0.1,0.2,0.6,0.7,0.75,0.8,0.85,0.9,0.95,1,1.05,1.1,1.15,1.2,1.25,1.3,1.35,1.4,1.5,1.6,1.8,2,4,8,16,32,64,128]
+        #As=[1+i*0.05 for i in range(32)]
+        #eps=0.001
+        #ct_tres=0.001
+        #refer to save_results.p (pickle'd performance_df2 with params above) for choice of parameters
+        # As=[1.15+i*0.01 for i in range(15)]
+        # exponents=[0.65+i*0.025 for i in range(18)]
+        # eps=0.0001
+        # ct_tres=0.0001
+        #refer to save_results_2.p (pickle'd performance_df2 with params above) for choice of parameters
+        As=[1.2]
+        exponents=[0.725]
+        eps=0.0001
+        ct_tres=0.0001
+        for A,exponent in itertools.product(As,exponents):
+            multi=4.185976
+            def chord_function(r, blade_span): return multi*(A-(r/blade_span)**(exponent))
 
-    slopes=[2.9+0.01*i for i in range(5)]
-    tip_chords=[0.95+0.005*i for i in range(5)]
+            blade_element_model = BladeElementModel(
+            blade_span=50,
+            blade_start=0.2*50,
+            blade_pitch=2,
+            blade_number=3,
+            twist=twist_function,
+            chord=chord_function,
+            airfoil_data=airfoil
+            )
 
-    for slope,tip_chord in itertools.product(slopes,tip_chords):
-       def chord_function(r, blade_span): return slope*(1-r/blade_span)+tip_chord
+            power_coefficient, thrust_coefficient , section_loads = blade_element_model.run_performance_calculations(
+            free_stream_velocity=inflow_speed,
+            tip_speed_ratio=tip_speed_ratio,
+            rotor_yaw=yaw_angle,
+            show_plots=True,
+            # save_plots_dir='./plots'
+            air_density=1.225
+            )
+            while abs(thrust_coefficient-0.75)>ct_tres:
+                multi=multi+eps
+                def chord_function(r, blade_span): return multi*(A-(r/blade_span)**(exponent))
 
-       blade_element_model = BladeElementModel(
-       blade_span=50,
-       blade_start=0.2*50,
-       blade_pitch=2,
-       blade_number=3,
-       twist=twist_function,
-       chord=chord_function,
-       airfoil_data=airfoil
-       )
-    
-    
-       power_coefficient, thrust_coefficient , section_loads = blade_element_model.run_performance_calculations(
-       free_stream_velocity=inflow_speed,
-       tip_speed_ratio=tip_speed_ratio,
-       rotor_yaw=yaw_angle,
-       show_plots=True,
-       # save_plots_dir='./plots'
-       air_density=1.225
-       )
-    
-       results.append({
-       'inflow_speed': inflow_speed,
-       'tip_speed_ratio': tip_speed_ratio,
-       'yaw_angle': yaw_angle,
-       'power_coefficient': power_coefficient,
-       'thrust_coefficient': thrust_coefficient,
-       'slope': slope,
-       'tip_chord': tip_chord,
-#       'Inflow_angle': section_loads['Inflow_angle'],
-#       'Alpha': section_loads['Alpha']
-       })
-    
-       performance_df2 = pd.DataFrame(results)
-       print(performance_df2[abs(performance_df2['thrust_coefficient']-0.75)<0.05])
+                blade_element_model = BladeElementModel(
+                blade_span=50,
+                blade_start=0.2*50,
+                blade_pitch=2,
+                blade_number=3,
+                twist=twist_function,
+                chord=chord_function,
+                airfoil_data=airfoil
+                )
+
+                power_coefficient_new, thrust_coefficient_new , section_loads_new = blade_element_model.run_performance_calculations(
+                free_stream_velocity=inflow_speed,
+                tip_speed_ratio=tip_speed_ratio,
+                rotor_yaw=yaw_angle,
+                show_plots=True,
+                # save_plots_dir='./plots'
+                air_density=1.225
+                )
+
+                dct=(thrust_coefficient_new-thrust_coefficient)/eps
+                multi=multi-eps+(0.75-thrust_coefficient)/dct
+
+                power_coefficient, thrust_coefficient, section_loads = blade_element_model.run_performance_calculations(
+                free_stream_velocity=inflow_speed,
+                tip_speed_ratio=tip_speed_ratio,
+                rotor_yaw=yaw_angle,
+                show_plots=True,
+                # save_plots_dir='./plots'
+                air_density=1.225
+                )
+            print(power_coefficient)
+
+
+            
+            results.append({
+            'inflow_speed': inflow_speed,
+            'tip_speed_ratio': tip_speed_ratio,
+            'yaw_angle': yaw_angle,
+            'power_coefficient': power_coefficient,
+            'thrust_coefficient': thrust_coefficient,
+            'A': A,
+            'exp': exponent,
+            'multi': multi
+        #       'Inflow_angle': section_loads['Inflow_angle'],
+        #       'Alpha': section_loads['Alpha']
+            })
+        
+        performance_df2 = pd.DataFrame(results)
+        print(performance_df2)
+        print(max(performance_df2['power_coefficient']))
+        print(performance_df2[performance_df2['power_coefficient']>0.99*max(performance_df2['power_coefficient'])])
 print('break')
 
 
